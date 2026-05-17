27cd893073423c96756a197e414e0f0a611d8023
DieMT10 <167644619+DieMT10@users.noreply.github.com>
2026-05-16 23:32:51 -0600

    importar y configurar arcore-android-sdk.git a proyecto

---
+++ app/src/main/java/com/example/proyectoarpdm/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.proyectoarpdm;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.proyectoarpdm.helloar.HelloArActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        
+        // Use R.id.main (added to activity_main.xml)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Redirect to HelloArActivity to use the ARCore functionality
+        Intent intent = new Intent(this, HelloArActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
